a69af42734b83fe0dabdc6c938e8c5c540c9ac77
Mykola <n0ikolay@gmail.com>
2024-10-01 13:22:58 +0300

    created a new branche to dev and did homework

--- Program.cs
+++ Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TempAnaliz tempAnaliz = new TempAnaliz();
+
+            tempAnaliz.InputMonthlyTemp();
+
+            double yearAvrage = tempAnaliz.Culc();
+
+            Console.WriteLine("$ Temp of the year {yearAvrage: C }");
+
+            double[,] dailyTemp = tempAnaliz.GenDailyTemp();
+
+            double[] monthlyAverage = tempAnaliz.CalculateMonthlyAverages(dailyTemp);
+
+            Array.Sort(monthlyAverage);
+
+            Console.WriteLine("Average temp in mounth :");
+
+            foreach (var avg in monthlyAverage)
+            {
+                Console.WriteLine($"{avg:F2}C");
+            }
         }
     }
 }
---
+++ TempAnaliz.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TempAnaliz
+{
+    private const int MonthsInYear = 12; 
+    private const int DaysInMonth = 30;   
+    private LinkedList<double> monthlyTemp;
+
+    public TempAnaliz()
+    {
+        monthlyTemp = new LinkedList<double>();
+    }
+
+    public void InputMonthlyTemp()
+    {
+        for (int i = 0; i < MonthsInYear; i++)
+        {
+            Console.Write($"Enter average temp for month {i + 1}: ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            monthlyTemp.AddLast(temp);
+        }
+    }
+
+
+    public double Culc()
+    {
+        double sum = 0;
+        int count = 0;
+
+        foreach (var temp in monthlyTemp)
+        {
+            sum += temp;
+            count++;
+        }
+        return sum / count;
+    }
+
+    public double[,] GenDailyTemp()
+    {
+        double[,] temp = new double[MonthsInYear, DaysInMonth];
+        Random random = new Random();
+
+        for (int i = 0; i < MonthsInYear; i++)
+        {
+            for (int j = 0; j < DaysInMonth; j++)
+            {
+                temp[i, j] = random.Next(-10, 36);
+            }
+        }
+        return temp;
+    }
+
+    public double[] CalculateMonthlyAverages(double[,] temp)
+    {
+        double[] averages = new double[MonthsInYear];
+
+        for (int month = 0; month < MonthsInYear; month++)
+        {
+            double sum = 0;
+            for (int day = 0; day < DaysInMonth; day++)
+            {
+                sum += temp[month, day];
+            }
+            averages[month] = sum / DaysInMonth;
+        }
+
+        return averages;
+    }
+}
