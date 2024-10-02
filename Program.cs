aff48e11accf714f8cecf05a7f0e98f0d9d0cb20
Mykola <n0ikolay@gmail.com>
2024-10-02 14:09:44 +0300

    did gomework 5.2

--- Program.cs
+++ Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+        static (int, int) Swap(int a, int b)
+        {
+            return (b, a);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter first num");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second num");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Before swap : firstNum = {firstNum}, secondNum = {secondNum}");
+            (firstNum, secondNum) = Swap(firstNum, secondNum);
+
+            Console.WriteLine($"After swapped : firstNum = {firstNum}, secondNum = {secondNum}");
+
         }
     }
 }
+
+
+
