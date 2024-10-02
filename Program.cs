6012f470efc7ca91c45163b858fc0f2daee75309
Mykola <n0ikolay@gmail.com>
2024-10-02 13:12:05 +0300

    did homework 5.1

--- Program.cs
+++ Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+
+        static void FindMax(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter first num");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second num");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            int maxNum = Max(firstNum, secondNum);
+            Console.WriteLine("Grestes num is {maxnum}"});
         }
     }
 }
