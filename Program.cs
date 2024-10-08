19cb3fc7016e57cc4f5124729cef9aac2a62465f
Mykola <n0ikolay@gmail.com>
2024-10-08 13:51:44 +0300

    did homework 5.5

--- Program.cs
+++ Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+
+        public static int Fibonacci(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("n must be naturel number");
+            }
+
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n-2);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the num Fibonacci");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                int result = Fibonacci(n);
+                Console.WriteLine($"The Fibonacci number is {n} equals : {result}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
