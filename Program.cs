03b48d7eb40620c261c9b20895ad554095979118
Mykola <n0ikolay@gmail.com>
2024-10-10 16:18:55 +0300

    did homework6.3.1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ResidentialBuilding residentialBuilding = new ResidentialBuilding(60, 20, 100, 4, true);
+            residentialBuilding.PrintBuildingInfo();
+
+            Console.WriteLine();
+
+            ResidentialBuilding residentialBuilding2 = new ResidentialBuilding(50, 20, 80, 3, false);
+            residentialBuilding2.PrintBuildingInfo();
         }
     }
 }
---
+++ ResidentialBuilding.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class ResidentialBuilding : Building
+{
+    private bool hasYard;
+
+    public ResidentialBuilding(double height, int floors, int apartments, int entrances, bool hasYard)
+         : base(height, floors, apartments, entrances)
+    {
+        this.hasYard = hasYard;
+    }
+
+    public override void PrintSpecificInfo()
+    {
+        Console.WriteLine($"Residential building with yard: {hasYard}");
+    }
+
+    public override void PrintBuildingInfo()
+    {
+        base.PrintBuildingInfo();
+    }
+}
